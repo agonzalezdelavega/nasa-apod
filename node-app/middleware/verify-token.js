0ba34857933e58393346c0f2edd04b96e91579d8
agonzalezdelavega <albertogdelavega@gmail.com>
2023-07-26 15:10:50 -0500

    Added refresh token functionality

--- node-app/middleware/verify-token.js
+++ node-app/middleware/verify-token.js
@@ -1,7 +1,27 @@
+const { CognitoIdentityProviderClient, InitiateAuthCommand } = require("@aws-sdk/client-cognito-identity-provider");
 const { CognitoJwtVerifier } = require("aws-jwt-verify");
+
+const client = new CognitoIdentityProviderClient();
     
 module.exports = (req, res, next) => {
     if (req.session.accessToken) {
+        const currentTime = new Date();
+        req.session.cookie._expires = new Date(1658865607000);
+        if (currentTime > req.session.cookie._expires) {
+            (async() => {
+                const input = {
+                    AuthFlow: "REFRESH_TOKEN_AUTH",
+                    AuthParameters: {"REFRESH_TOKEN": req.session.refreshToken},
+                    ClientId: process.env.COGNITO_CLIENT_ID
+                };
+                const command = new InitiateAuthCommand(input);
+                const response = await client.send(command)
+                .then((response) => {
+                    req.session.accessToken = response.AuthenticationResult.AccessToken;
+                });
+            })();
+        };
+
         (async() => {
             const verifier = CognitoJwtVerifier.create({
                 userPoolId: process.env.USER_POOL_ID,
@@ -11,6 +31,8 @@ module.exports = (req, res, next) => {
 
             const payload = await verifier.verify(req.session.accessToken)
             .then((response) => {
+                exp = new Date(response.exp*1000);
+                req.session.cookie.expires = exp;
                 res.locals.userid = response.username;
                 next();
             });
