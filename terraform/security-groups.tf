25f87115d6aba61e0f7ed03fbb9b88f19af253e1
agonzalezdelavega <albertogdelavega@gmail.com>
2023-08-15 02:53:45 -0500

    Revised SGs

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -9,19 +9,7 @@ resource "aws_security_group" "elb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
@@ -31,7 +19,10 @@ resource "aws_security_group" "elb" {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [
+      aws_security_group.nat.id,
+      aws_security_group.ecs_service.id
+    ]
   }
 }
 
@@ -41,30 +32,6 @@ resource "aws_security_group" "nat" {
   name   = "${local.prefix}-nat"
   vpc_id = aws_vpc.main.id
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block]
-  }
-  egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block]
-  }
-  egress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block]
-  }
-  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
@@ -78,30 +45,47 @@ resource "aws_security_group" "nat" {
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "nat_ingress_elb" {
+  security_group_id = aws_security_group.nat.id
+  referenced_security_group_id = aws_security_group.elb.id
+  from_port   = 3000
+  to_port     = 3000
+  ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "nat_ingress_ecs" {
+  security_group_id = aws_security_group.nat.id
+  referenced_security_group_id = aws_security_group.ecs_service.id
+  from_port   = 3000
+  to_port     = 3000
+  ip_protocol = "tcp"
+}
+
 # ECS
 
 resource "aws_security_group" "ecs_service" {
   name   = "${local.prefix}-ecs"
   vpc_id = aws_vpc.main.id
-  ingress {
-    from_port = 3000
-    to_port   = 3000
-    protocol  = "tcp"
-    security_groups = [
-      aws_security_group.elb.id,
-      aws_security_group.nat.id
-    ]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   egress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.nat.id]
   }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ecs_ingress_elb" {
+  security_group_id = aws_security_group.ecs_service.id
+  referenced_security_group_id = aws_security_group.elb.id
+  from_port   = 3000
+  to_port     = 3000
+  ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ecs_ingress_nat" {
+  security_group_id = aws_security_group.ecs_service.id
+  referenced_security_group_id = aws_security_group.nat.id
+  from_port   = 3000
+  to_port     = 3000
+  ip_protocol = "tcp"
 }
